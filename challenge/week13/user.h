3ef4f9075c351bb8bc734a4853796a27154ec09e
junseok kim <try98007096@gmail.com>
2023-11-27 15:35:03 +0900

    Week13. 머드 게임 확장

---
+++ challenge/week13/user.h
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class User {
+
+private:
+	int hp; // 유저의 체력
+
+public:
+	int x = 0; // 유저의 x좌표
+	int y = 0; // 유저의 y좌표
+
+	User();
+
+	void DecreaseHP(int dec_hp);
+	int GetHP();
+	void IncreaseHP(int inc_hp);
+	void ShowHealth();
+	virtual void DoAttack();
+};
+
+class Magician : public User {
+
+public :
+	Magician();
+
+	void DoAttack() override;
+};
+
+class Warrior : public User {
+
+public:
+	Warrior();
+
+	void DoAttack() override;
+};
